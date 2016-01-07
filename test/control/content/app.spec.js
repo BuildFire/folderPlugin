984f00941b69f71ae8a9c30e63cdea25b54cbb25
Zain <zelabedeen@madaincorp.com>
2016-01-07 20:18:59 +0200

    Created test case environment(Karma) & directory structure and added

---
+++ test/control/content/app.spec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zain on 05/01/16.
+ */
+
+describe('opentablePlugin Content: app', function () {
+
+    describe('Buildfire service', function () {
+        it('Buildfire should exists', function () {
+            expect(true).toBeDefined();
+        });
+    });
+});
