17d13450e73a9952405d7db19ac1a71e81bb96be
Vishnu Singh <vish250491@gmail.com>
2016-04-20 16:27:13 +0530

    layout12 height removbed

--- widget/widget.controller.js
+++ widget/widget.controller.js
@@ -444,7 +444,7 @@ folderPluginApp.controller('folderPluginCtrl', ['$scope', '$sce','$timeout', fun
 
     $scope.$on('LastRepeaterElement', function(){
        // $('.plugin-slider.text-center.owl-carousel').trigger("destroy.owl.carousel");
-        $scope.layout12Height= $('.plugin-slider .plugin-slide').first().height()+17+'px';
+        $scope.layout12Height= $('.plugin-slider .plugin-slide').first().height()+'px';
             var slides = $('.plugin-slider .plugin-slide').length;
         $scope.layout12TotalItem=$scope.layout12TotalItem+1;
             // Slider needs at least 2 slides or you'll get an error.
